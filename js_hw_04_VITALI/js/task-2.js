c196d02207d964e91b0776bf131648721d7bdde4
viiita <80007258+viiita@users.noreply.github.com>
2021-04-20 10:11:31 +0300

    Add files via upload

---
+++ js_hw_04_VITALI/js/task-2.js
@@ -0,0 +1,34 @@
+/* Задание 2
+Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+ */
+
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking ${action.name} on ${itemName}`);
+  action.call(inventory, itemName);
+};
+invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
+
+
+
